dc280f002b2aea87cfa9fbcb89a91741855e9140
Jack Kamm <jackkamm@gmail.com>
2024-10-15 14:57:29 +0900

    Partially revert last commit

--- lisp/ob-R.el
+++ lisp/ob-R.el
@@ -375,15 +375,11 @@ Has four %s escapes to be filled in:
     (session body result-type result-params column-names-p row-names-p async)
   "Evaluate R code in BODY."
   (if session
-      (progn
-        (with-current-buffer session
-          (setq org-babel-comint-prompt-regexp-override
-                (concat "^" comint-prompt-regexp)))
-        (if async
-            (ob-session-async-org-babel-R-evaluate-session
-             session body result-type column-names-p row-names-p)
-          (org-babel-R-evaluate-session
-           session body result-type result-params column-names-p row-names-p)))
+      (if async
+          (ob-session-async-org-babel-R-evaluate-session
+           session body result-type column-names-p row-names-p)
+        (org-babel-R-evaluate-session
+         session body result-type result-params column-names-p row-names-p))
     (org-babel-R-evaluate-external-process
      body result-type result-params column-names-p row-names-p)))
 
@@ -460,11 +456,12 @@ last statement in BODY, as elisp."
 		     (substring line (match-end 1))
 		   line))
 	       (with-current-buffer session
-		 (org-babel-comint-with-output (session org-babel-R-eoe-output)
-		   (insert (mapconcat 'org-babel-chomp
-				      (list body org-babel-R-eoe-indicator)
-				      "\n"))
-		   (inferior-ess-send-input))))))) "\n"))))
+		 (let ((comint-prompt-regexp (concat "^" comint-prompt-regexp)))
+		   (org-babel-comint-with-output (session org-babel-R-eoe-output)
+		     (insert (mapconcat 'org-babel-chomp
+					(list body org-babel-R-eoe-indicator)
+					"\n"))
+		     (inferior-ess-send-input)))))))) "\n"))))
 
 (defun org-babel-R-process-value-result (result column-names-p)
   "R-specific processing of return value.
--- lisp/ob-comint.el
+++ lisp/ob-comint.el
@@ -75,17 +75,11 @@ This is useful when prompt unexpectedly changes."
       (setq comint-prompt-regexp org-babel-comint-prompt-regexp-old
             org-babel-comint-prompt-regexp-old tmp))))
 
-(defvar-local org-babel-comint-prompt-regexp-override nil
-  "Overrides `comint-prompt-regexp' in `org-babel-comint--prompt-filter.'")
-
 (defun org-babel-comint--prompt-filter (string &optional prompt-regexp)
   "Remove PROMPT-REGEXP from STRING.
 
-PROMPT-REGEXP defaults to `comint-prompt-regexp', which can be
-overridden with `org-babel-comint-prompt-regexp-override'."
-  (let* ((prompt-regexp (or prompt-regexp
-                            org-babel-comint-prompt-regexp-override
-                            comint-prompt-regexp))
+PROMPT-REGEXP defaults to `comint-prompt-regexp'."
+  (let* ((prompt-regexp (or prompt-regexp comint-prompt-regexp))
          ;; We need newline in case if we do progressive replacement
          ;; of agglomerated comint prompts with `comint-prompt-regexp'
          ;; containing ^.
