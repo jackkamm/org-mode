0c57b547b2317dc7ea592b7a9d54a704291a295f
Jack Kamm <jackkamm@gmail.com>
2024-10-15 15:13:51 +0900

    Add variable to inhibit prompt removal

--- lisp/ob-R.el
+++ lisp/ob-R.el
@@ -486,7 +486,8 @@ by `org-babel-comint-async-filter'."
    session (current-buffer)
    "^\\(?:[>.+] \\)*\\[1\\] \"ob_comint_async_R_\\(start\\|end\\|file\\)_\\(.+\\)\"$"
    'org-babel-chomp
-   'ob-session-async-R-value-callback)
+   'ob-session-async-R-value-callback
+   t)
   (cl-case result-type
     (value
      (let ((tmp-file (org-babel-temp-file "R-")))
